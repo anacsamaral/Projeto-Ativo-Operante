13264ae390c56f57610838646b8297a4cc1eeaf8
Ana Amaral <anacarolinesousadoamaral2020@gmail.com>
2026-04-27 14:23:52 -0300

    Adicionar repositórios

--- src/main/java/unoeste/fipp/projetoativooperante/repositories/UsuarioRepository.java
+++ src/main/java/unoeste/fipp/projetoativooperante/repositories/UsuarioRepository.java
@@ -1,17 +1,4 @@
 package unoeste.fipp.projetoativooperante.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import unoeste.fipp.projetoativooperante.entities.Usuario;
-
-import java.util.List;
-
-@Repository
-public interface class UsuarioRepository extends JpaRepository<Usuario, Long> {
-    public Usuario findByNome(String nome);
-
-    @Query(value = "SELECT * FROM usuario WHERE upper(usu_cpf) LIKE %:keyword%", nativeQuery = true)
-    public List<Usuario> findByKW(@Param("keyword") String keyword);
+public interface UsuarioRepository {
 }
